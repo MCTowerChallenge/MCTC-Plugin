78c17d14f01e80220658e91536d6c9735396af0b
Idkahn <jared@fineonline.com>
2022-08-02 02:41:17 -0700

    removed TowerBlock.java in favor of using BlockState

--- src/main/java/io/github/idkahn/towerchallenge/Towering/TowerListener.java
+++ src/main/java/io/github/idkahn/towerchallenge/Towering/TowerListener.java
@@ -1,98 +1,104 @@
 package io.github.idkahn.towerchallenge.Towering;
 
 import io.github.idkahn.towerchallenge.TeamColors;
-import net.kyori.adventure.audience.Audience;
-import net.kyori.adventure.audience.ForwardingAudience;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextComponent;
 import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
 import org.bukkit.block.BlockState;
 import org.bukkit.block.ShulkerBox;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
-import org.bukkit.plugin.java.JavaPlugin;
-import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
 import java.util.Iterator;
-import java.util.function.Predicate;
 
 public class TowerListener implements Listener {
 
-    private ArrayList<TowerBlock>[] blocks;
+    private ArrayList<BlockState>[] blocks;
     private boolean towering;
 
     public TowerListener() {
         this.towering = false;
         this.blocks = new ArrayList[TeamColors.values().length];
         for (int i = 0; i < blocks.length; i++) {
-            blocks[i] = new ArrayList<TowerBlock>();
+            blocks[i] = new ArrayList<BlockState>();
         }
     }
 
     @EventHandler
     public void onPlaceBlock(BlockPlaceEvent event) {
 
-        Block placedBlock = event.getBlockPlaced();
-        BlockState placedBlockState = placedBlock.getState();
-
-        if (placedBlockState instanceof ShulkerBox) {
-            event.getPlayer().sendMessage("Shulker Box!");
-            ShulkerBox shulkerBox = (ShulkerBox) placedBlockState;
-
-            if (shulkerBox.customName() != null) {
-                event.getPlayer().sendMessage(shulkerBox.customName());
-            }
-        }
+        Player player = event.getPlayer();
+        BlockState placedBlockState = event.getBlockPlaced().getState();
 
         if (towering) {
 
-            for (ArrayList<TowerBlock> array : blocks) {
-                for (TowerBlock block : array) {
-                    if (placedBlock.getType() == block.getBlockType()) {
-                        event.setCancelled(true);
+            if (placedBlockState instanceof ShulkerBox) {
+//                event.getPlayer().sendMessage("Shulker Box!");
+                ShulkerBox shulkerBox = (ShulkerBox) placedBlockState;
 
-                        TextComponent text = Component.text("You have already placed ").
-                                append(Component.text(placedBlock.getType().name(), NamedTextColor.RED))
-                                .append(Component.text("!"));
+                if (shulkerBox.customName() != null) {
+                    String shulkerBoxName = PlainTextComponentSerializer.plainText().serialize(shulkerBox.customName());
+    //                player.sendMessage(shulkerBoxName);
 
-                        event.getPlayer().sendActionBar(text);
-//                        event.getPlayer().sendActionBar("You have already placed that block!");
+                    if (shulkerBoxName.equals("Starting Shulker")) {
+//                        player.sendMessage(shulkerBox.customName());
                         return;
                     }
                 }
             }
 
-            blocks[0].add(new TowerBlock(event.getBlockPlaced()));
-            event.getPlayer().sendMessage("Blocks Placed: " + blocks[0].size());
+            for (ArrayList<BlockState> array : blocks) {
+                for (BlockState blockState : array) {
+                    if (placedBlockState.getType() == blockState.getType()) {
+                        event.setCancelled(true);
+
+                        TextComponent text = Component.text("You have already placed ").
+                                append(Component.text(placedBlockState.getType().name(), NamedTextColor.RED))
+                                .append(Component.text("!"));
+
+                        player.sendActionBar(text);
+                        return;
+                    }
+                }
+            }
+
+            blocks[0].add(placedBlockState);
+            player.sendMessage("Blocks Placed: " + blocks[0].size());
         }
 
-
     }
+
+
 
     @EventHandler
     public void onBreakBlock(BlockBreakEvent event) {
 
+        Player player = event.getPlayer();
+        BlockState brokenBlockState = event.getBlock().getState();
+
         if (towering) {
-            for (ArrayList<TowerBlock> list : blocks) {
-                Iterator<TowerBlock> listIterator = list.iterator();
+            for (ArrayList<BlockState> list : blocks) {
+                Iterator<BlockState> listIterator = list.iterator();
                 while (listIterator.hasNext()) {
-                    TowerBlock block = listIterator.next();
+                    BlockState blockState = listIterator.next();
 
-                    Location blockLocation = block.getLocation();
+                    Location blockLocation = blockState.getLocation();
 
-                    if (blockLocation.getX() == event.getBlock().getX() &&
-                            blockLocation.getY() == event.getBlock().getY() &&
-                            blockLocation.getZ() == event.getBlock().getZ()) {
+                    if (blockLocation.getX() == brokenBlockState.getX() &&
+                            blockLocation.getY() == brokenBlockState.getY() &&
+                            blockLocation.getZ() == brokenBlockState.getZ()) {
 
     //                    event.getPlayer().sendMessage(event.getBlock().getType().name());
                         listIterator.remove();
-                        event.getPlayer().sendMessage("Blocks Placed: " + blocks[0].size());
+                        player.sendMessage("Blocks Placed: " + blocks[0].size());
                         return;
                     }
 
@@ -111,11 +117,11 @@ public class TowerListener implements Listener {
     }
 
     public void removeBlocks() {
-        for (ArrayList<TowerBlock> list : blocks) {
-            Iterator<TowerBlock> listIterator = list.iterator();
+        for (ArrayList<BlockState> list : blocks) {
+            Iterator<BlockState> listIterator = list.iterator();
             while (listIterator.hasNext()) {
-                TowerBlock block = listIterator.next();
-                block.getBlock().setType(Material.AIR);
+                BlockState blockState = listIterator.next();
+                blockState.getBlock().setType(Material.AIR);
                 listIterator.remove();
             }
         }
